ca8055b9124a5d4047e44729a17d4899b58b1abb
Guilherme Tozzi Mafra <80405986+GuilhermeTozziMafra@users.noreply.github.com>
2024-06-14 16:40:31 -0300

    Add files via upload

---
+++ Formulários WEB/Preenchendo Formulário WEB aula 2.py
@@ -0,0 +1,87 @@
+from selenium import webdriver as opcoesSelenium
+from selenium.webdriver.common.by import By
+import pyautogui
+
+from selenium.webdriver.support.select import Select
+
+navegador = opcoesSelenium.Chrome()
+
+navegador.get("https://form.jotform.com/221436066464051")
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(4)
+
+#Preenche o campo Nome
+navegador.find_element(By.NAME, "q3_nome[first]").send_keys("Ana Paula")
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(2)
+
+navegador.find_element(By.NAME, "q3_nome[last]").send_keys("Souza")
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(2)
+
+navegador.find_element(By.NAME, "q4_email").send_keys("ana.paula@hotmail.com")
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(2)
+
+pegaDropDown = navegador.find_element(By.ID, "input_5")
+itemSelecinado = Select(pegaDropDown)
+itemSelecinado.select_by_index(2)
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(3)
+
+filho = "Não"
+
+if filho == "Sim":
+
+    #Marca a opção te filho / SIM
+    navegador.find_element(By.ID, "label_input_6_0").click()
+
+else:
+    #Marca a opção te filho / SIM
+    navegador.find_element(By.ID, "label_input_6_1").click()
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(4)
+
+#Marca a opção cor Verde
+navegador.find_element(By.ID, "label_input_7_5").click()
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(2)
+
+#Marca a opção cor Laranja
+navegador.find_element(By.ID,"label_input_7_3").click()
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(2)
+
+#Marquei 5 estrelas
+navegador.find_element(By.XPATH, '//*[@id="input_8"]/div[5]').click()
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(3)
+
+#Marca como muito satisfeito
+navegador.find_element(By.ID, "input_9_0_3").click()
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(3)
+
+#Marca como satisfeito o Grau de Dificuldade
+navegador.find_element(By.ID, "input_9_1_2").click()
+
+#Tempo para o computador abrir o formulário
+pyautogui.sleep(3)
+
+#Enviar informações
+navegador.find_element(By.ID, "input_2").click()
+
+
+
+
+
